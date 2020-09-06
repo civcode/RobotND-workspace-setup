4f278d89a881baae4ef6a2bf34c2f8cd55a19cfb
civ <ivi3001@yahoo.de>
2020-09-06 16:00:33 +0200

    added script file

---
+++ upate_bashrc.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+PATH=$HOME/.bashrc
+#PATH=$HOME/Documents/RobotND-workspace-setup/test.txt
+
+echo "" >> $PATH
+echo "#=== my stuff ===" >> $PATH 
+
+echo "" >> $PATH
+echo "alias ll='ls -CF'" >> $PATH
+echo "alias l='ls -lah'" >> $PATH
+echo "alias 'cd..'='cd ..'" >> $PATH
+
+echo "" >> $PATH
+echo "alias ss='source devel/setup.bash'" >> $PATH
+
+#export MYVIMRC='/home/workspace/.vimrc'
+#export VIMINIT='source $MYVIMRC'
+
+echo "" >> $PATH
+echo 'git config --global user.email "ivi3001@yahoo.de"' >> $PATH
+
+# configure xterm appearance
+#xrdb /home/workspace/.Xresources
+
+echo "" >> $PATH
+echo "# make bash autocomplete write every suggestion in a separate line" >> $PATH
+echo "bind 'set completion-display-width 0'" >> $PATH

2571a6b4893b5e1cba9352c451fa73ad5764806a
chris <ivi3001@yahoo.de>
2021-05-13 22:37:22 +0200

    added basis package install script

---
+++ basic_packages.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y htop vim slurm mosh tmux ncdu
+sudo apt-get install -y openssh-server
+sudo apt-get install -y mesa-utils
+sudo apt-get install -y build-essential git cmake cmake-curses-gui
+sudo apt-get install -y wireshark
+sudo apt-get install -y v4l-utils
+
